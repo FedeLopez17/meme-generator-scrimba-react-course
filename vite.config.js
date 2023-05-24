c863ccb3025c501f425764cd72e9a89ec995731a
FedeLopez17 <fedelopez1712@gmail.com>
2023-05-24 17:59:40 -0300

    Fix paths after having renamed the repo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/meme-generator-scrimba-react-course/",
+  base: "/scrimba-react-project-3/",
   plugins: [react()],
 });
